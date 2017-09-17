afe94a8652d097014089f5d7624acf315f1b82e9
huuthang <phamhuuthang7697@gmail.com>
2017-09-17 22:16:14 +0700

    Them man hinh cap nhat thong tin ca nhanh

---
+++ app/src/main/java/srt/studentmanage/ui/activities/CapNhatTTCaNhanActivity.java
@@ -0,0 +1,15 @@
+package srt.studentmanage.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import srt.studentmanage.R;
+
+public class CapNhatTTCaNhanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cap_nhat_ttca_nhan);
+    }
+}
