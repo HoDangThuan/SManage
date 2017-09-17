e5171359a679ed2e65638d5e11a3c436d6d2e760
huuthang <phamhuuthang7697@gmail.com>
2017-09-17 22:18:19 +0700

    them man hinh cap nhat tai khoan ngan hang

---
+++ app/src/main/java/srt/studentmanage/ui/activities/CapNhatTkNganHangActivity.java
@@ -0,0 +1,15 @@
+package srt.studentmanage.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import srt.studentmanage.R;
+
+public class CapNhatTkNganHangActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cap_nhat_tk_ngan_hang);
+    }
+}
